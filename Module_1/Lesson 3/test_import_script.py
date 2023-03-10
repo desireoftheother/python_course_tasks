df2e7be892f513abf11168fb6127f4793e4e5efa
illia-teacher <illya.khoroshykh@gmail.com>
2023-03-10 20:47:23 +0200

    Added 4th task.
    Adjusted some files in project

--- Module_1/Lesson 3/test_import_script.py
+++ Module_1/Lesson 3/test_import_script.py
@@ -1,5 +1,7 @@
-import fake_random
+import random
 import example_module
 
 print(f"This is the example_modules __name__ var: {example_module.__name__}")
 print(f"This var is acessible both inside this module and example_module, here is its value: {example_module.example_var}")
+print(f"This is a random var:{random.random():10.4f}")
+print(f"This is a dir() call for random module: {dir(random)}")
