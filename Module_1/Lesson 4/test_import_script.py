a7afe35f2ac4ac315f13ea61f5b355ab137cb122
illia-teacher <illya.khoroshykh@gmail.com>
2023-03-30 18:43:09 +0300

    Added wide description for team work on lesson

--- Module_1/Lesson 4/test_import_script.py
+++ Module_1/Lesson 4/test_import_script.py
@@ -1,10 +1,3 @@
-import random
-import example_module
-from ..example_module_2 import ex_var_2
+import sys
 
-print(f"This is the example_modules __name__ var: {__name__}")
-print(f"This var is acessible both inside this module and example_module, here is its value: {example_module.example_var}")
-print(f"This is a random var:{random.random():10.4f}")
-print(f"This is a dir() call for random module: {dir(random)}")
-print(example_module.example_var)
-print(ex_var_2)
+print(sys.path)
