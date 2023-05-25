a93d417105334b7a4ed221de64b91c3246f3dad6
Illia Khoroshykh <ikhoroshykh@ironscales.com>
2023-05-25 18:44:29 +0300

    Client and server drafts

---
+++ module_4/32_osi_model_sockets/client.py
@@ -0,0 +1,12 @@
+import socket
+
+HOST = "127.0.0.1" #This should be the PUBLIC IP of the server
+PORT = 65432
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    while True:
+        inp = bytes(input(), "utf-8")
+        s.sendall(inp)
+        data = s.recv(1024)
+        print("received", repr(data))
---
+++ module_4/32_osi_model_sockets/server.py
@@ -0,0 +1,26 @@
+import socket
+
+HOST = "127.0.0.1" #This should be the PRIVATE IP of the server
+PORT = 65432
+
+sock = socket.socket()
+sock.bind((HOST, PORT))
+
+sock.listen(1)
+
+# Uncomment to mack socket unblocking later
+# sock.setblocking(False)
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data.upper())
+
